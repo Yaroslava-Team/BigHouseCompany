8465667186e83587c1f7e7b4acd407b26f00a6b9
yaroslavaaaa935-art <yaroslavaaaa935@gmail.com>
2026-03-14 21:37:49 +0300

    Добавлена адаптивность

--- bighouse/js/main.js
+++ bighouse/js/main.js
@@ -1,7 +1,34 @@
 const faqItems = document.querySelectorAll('.faq-item');
+// Бургер-меню
+const burger = document.getElementById('burgerIcon');
+const nav = document.querySelector('.nav');
+
 
 faqItems.forEach(item => {
   item.addEventListener('click', () => {
     item.classList.toggle('active');
   });
 });
+
+
+if (burger && nav) {
+  burger.addEventListener('click', (e) => {
+    e.stopPropagation();
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+
+  nav.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      burger.classList.remove('active');
+      nav.classList.remove('active');
+    });
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!nav.contains(e.target) && !burger.contains(e.target)) {
+      burger.classList.remove('active');
+      nav.classList.remove('active');
+    }
+  });
+}
